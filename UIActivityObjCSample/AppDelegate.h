defacf7ff157a9f5eaadff12a150153242e1f819
monkey <gikeda@appirits.com>
2017-11-29 11:43:42 +0900

    Initial Commit

---
+++ UIActivityObjCSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIActivityObjCSample
+//
+//  Created by monkey on 2017/11/29.
+//  Copyright © 2017年 monkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIActivityObjCSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIActivityObjCSample
+//
+//  Created by monkey on 2017/11/29.
+//  Copyright © 2017年 monkey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
